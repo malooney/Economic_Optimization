8b135c07a45504230c1a72b9bf510b2d5719f020
malooney <matthew.looney@icloud.com>
2017-12-02 09:47:41 -0600

    pres. and paper

--- Energy_Optim/MC_Study_general_Dist.R
+++ Energy_Optim/MC_Study_general_Dist.R
@@ -10,12 +10,16 @@ source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimizat
 source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimization/Economic_Optimization/Energy_Optim/LCOE_NGCC.R")
 source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimization/Economic_Optimization/Energy_Optim/LCOE_NG_PEAKING.R")
 source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimization/Economic_Optimization/Energy_Optim/LCOE_NUCLEAR.R")
+source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimization/Economic_Optimization/Energy_Optim/LCOE_WIND.R")
+source("/Users/malooney/Google Drive/digitalLibrary/*AAEC6305_Economic_Optimization/Economic_Optimization/Energy_Optim/LCOE_SOLAR_PV.R")
 
-mu <- c(0,0,0,0)
-Sigma <- matrix(c(1, 0.5, 0.5, 0.5,
-                  0.5, 1, 0.5, 0.5,
-                  0.5, 0.5, 1, 0.5,
-                  0.5, 0.5, 0.5, 1), nrow=4)
+mu <- c(0,0,0,0,0,0)
+Sigma <- matrix(c(1, 0.5, 0.5, 0.5, 0.5, 0.5,
+                  0.5, 1, 0.5, 0.5, 0.5, 0.5,
+                  0.5, 0.5, 1, 0.5, 0.5, 0.5,
+                  0.5, 0.5, 0.5, 1, 0.5, 0.5,
+                  0.5, 0.5, 0.5, 0.5, 1, 0.5,
+                  0.5, 0.5, 0.5, 0.5, 0.5, 1), nrow=6)
 
 e <- mvrnorm(10000, mu, Sigma)
 
@@ -23,26 +27,37 @@ LCOE_c <- LCOE_COAL()
 LCOE_ngcc <- LCOE_NGCC()
 LCOE_ng_peaking <- LCOE_NG_PEAKING()
 LCOE_nuclear <- LCOE_NUCLEAR()
+LCOE_wind <- LCOE_WIND()
+LCOE_solar_pv <- LCOE_SOLAR_PV()
 
 LCOE_c <- matrix(LCOE_c+ e[,1], ncol=1)
 LCOE_ngcc <- matrix(LCOE_ngcc+ e[,2], ncol=1)
 LCOE_ng_peaking <- matrix(LCOE_ng_peaking+ e[,3], ncol=1)
 LCOE_nuclear <- matrix(LCOE_nuclear+ e[,4], ncol=1)
+LCOE_wind <- matrix(LCOE_wind+ e[,5], ncol=1)
+LCOE_solar_pv <- matrix(LCOE_solar_pv+ e[,6], ncol=1)
 
 
 sqrt(var(LCOE_c))
 sqrt(var(LCOE_ngcc))
 sqrt(var(LCOE_ng_peaking))
 sqrt(var(LCOE_nuclear))
+sqrt(var(LCOE_wind))
+sqrt(var(LCOE_solar_pv))
 
-vcov_matrix <- var(cbind(LCOE_c, LCOE_ngcc, LCOE_ng_peaking, LCOE_nuclear))
+data_matrix <- data.frame(cbind(LCOE_c, LCOE_ngcc, LCOE_ng_peaking, LCOE_nuclear, LCOE_wind, LCOE_solar_pv))
 
 plot(density(LCOE_c))
 plot(density(LCOE_ngcc))
 plot(density(LCOE_ng_peaking))
 plot(density(LCOE_nuclear))
+plot(density(LCOE_wind))
+plot(density(LCOE_solar_pv))
+hist(LCOE_solar_pv)
 
 summary(LCOE_c)
 summary(LCOE_ngcc)
 summary(LCOE_ng_peaking)
 summary(LCOE_nuclear)
+summary(LCOE_wind)
+summary(LCOE_solar_pv)
